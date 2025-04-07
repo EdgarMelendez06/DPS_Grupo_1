2ac424f934a9082351c3b7758ea919cdcb832598
EdgarMelendez06 <GM240279@alumno.udb.edu.sv>
2025-04-06 22:18:21 -0600

    Delete components directory

--- components/ARScene.js
+++
@@ -1,9 +0,0 @@
-import SensorPanelAR from './SensorPanelAR';
-
-// Dentro de tu ARSceneComponent:
-<SensorPanelAR
-  sensorData={sensorData}
-  onUpdate={handleUpdate}
-  onToggleModel={() => setShowModel(!showModel)}
-  isLoading={isUpdating}
-/>
